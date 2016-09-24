30810e7be238828ddcfdd0721b75539ec763ea5c
Sebastian Rios <seba.rios@gmail.com>
2016-09-24 08:30:32 -0300

    debug recipe

--- app.js
+++ app.js
@@ -157,31 +157,19 @@ router.post('/', function(req,res) {
    if (login_recipe = req.body.login) {
        try {
             selector = 'Creating Nightmare instance';
-            cookies = req.body.cookies;
             var nm_opts = {
                 show:true,
                 waitTimeout: 3000,
                 'ignore-certificate-errors': true
             };
-            // if (!cookies){
-                // nm_opts['paths'] = { userData: '/Users/matrix/Proyectos/pulpou/pulpou-reports/pulpou-nightmare/userData/'+login_recipe['isLoggedIn']['value']};
-                nm_opts['paths'] = { userData: '/app/userData/'+login_recipe['isLoggedIn']['value']};
-            // }
+            nm_opts['paths'] = { userData: '/app/userData/'+login_recipe['isLoggedIn']['value']};
             nightmare = Nightmare(nm_opts);
-            // vo(login)(function(err, result) {
-                // if (err){
-                    // res.send({status: 'error', message: err});
-                // } else {
-                    // logged_in = result.logged_in;
                     logged_in = true;
                     recipe = req.body.recipe;
                     if (logged_in && recipe){
+                        console.log('recipe', util.inspect(recipe, {showHidden: false, depth: null}));
                         if (recipe.goto && recipe.response && recipe.evaluate){
                             try {
-                                // console.log('cookies:');
-                                // nightmare.cookies.get({ url: null },function(e,c){
-                                //     console.log(JSON.stringify(c));
-                                // });
                                 vo(run)(function(err, result) {
                                     try{
                                         if (err) res.status(400).send(err);
